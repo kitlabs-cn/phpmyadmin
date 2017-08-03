70085e811202fa3903729d817227a6494222990d
李春鹏 <lcp0578@gmail.com>
2017-08-03 10:27:52 +0800

    [MOD] init phpmyadmin 4.7.3

---
+++ vendor/phpmyadmin/motranslator/tests/PluralTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Test for gettext parsing.
+ */
+class PluralTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test for npgettext.
+     *
+     * @param int    $number   Number
+     * @param string $expected Expected output
+     *
+     *
+     * @dataProvider providerTestNpgettext
+     */
+    public function testNpgettext($number, $expected)
+    {
+        $parser = new PhpMyAdmin\MoTranslator\Translator(null);
+        $result = $parser->npgettext(
+            'context',
+            "%d pig went to the market\n",
+            "%d pigs went to the market\n",
+            $number
+        );
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * Data provider for test_npgettext.
+     *
+     * @return array
+     */
+    public static function providerTestNpgettext()
+    {
+        return array(
+            array(1, "%d pig went to the market\n"),
+            array(2, "%d pigs went to the market\n"),
+        );
+    }
+}
